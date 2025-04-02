96c023073b9730f6a74cad3ca5389eb1c8ca2cab
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-02 12:39:49 -0400

    changed colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 // Write your Color component here
 const Color = ({ color, setSelectedColor }) => {
   return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+
 };
 const App = () => {
   const [selectedColor, setSelectedColor] = useState("");
-
   return (
     <div id="container">
       <div id="navbar">
@@ -13,12 +13,11 @@ const App = () => {
         <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="orange" setSelectedColor={setSelectedColor}/>
-        <Color color="green" setSelectedColor={setSelectedColor}/>
         <Color color="blue" setSelectedColor={setSelectedColor}/>
+        <Color color="green" setSelectedColor={setSelectedColor}/>
+        <Color color="orange" setSelectedColor={setSelectedColor}/>
       </div>
     </div>
   );
 };
-
 export default App;
